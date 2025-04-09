ffd2c4209f22eec43c2a5bef74e53906051445fa
Med-arslen <MedArslen.Amira@esprit.tn>
2025-04-09 14:00:00 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $host = "localhost";
 $dbname = "projet_film";
 $username = "root";
 $password = "";
-
+//shgfqgy
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
